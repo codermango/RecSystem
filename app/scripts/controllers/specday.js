39ece556d5b1a5639c6dc94c321d4a85ecf8d266
codermango <codermango@gmail.com>
2015-12-10 16:05:08 +0100

    Mainly get data from backend for spec day

--- app/scripts/controllers/specday.js
+++ app/scripts/controllers/specday.js
@@ -1,20 +1,26 @@
 'use strict';
 angular.module("RecSystemWebApp")
 .controller("SpecdayCtrl", function ($scope, $http) {
-    $scope.selectionItems = [{
-        name: '0204',
-        value: '10 February (World Cancer Day)'
-    }, {
-        name: '0911',
-        value: '11 September (Terror)'
-    }, {
-        name: '1210',
-        value: '10 December (Nobel prize)'
-    }];
+    // $scope.selectionItems = [{
+    //     name: '0204',
+    //     value: '10 February (World Cancer Day)'
+    // }, {
+    //     name: '0911',
+    //     value: '11 September (Terror)'
+    // }, {
+    //     name: '1210',
+    //     value: '10 December (Nobel prize)'
+    // }];
 
-    $scope.selectedItem = $scope.selectionItems[0];
-    $http.get('http://127.0.0.1:8888/special_day/' + $scope.selectedItem.name).success(function(data) {
-        $scope.specdayMovies = data['specday_movies'];
+    $http.get('http://127.0.0.1:8888/holiday_keyword').success(function(data) {
+        $scope.selectionItems = [];
+        for (var key in data) {
+            $scope.selectionItems.push({name: key, value: key + ' ' + data[key]});
+        }
+        $scope.selectedItem = $scope.selectionItems[0];
+        $http.get('http://127.0.0.1:8888/special_day/' + $scope.selectedItem.name).success(function(data) {
+            $scope.specdayMovies = data['specday_movies'];
+        });
     });
 
     $scope.selectItem = function(selectedItem) {
