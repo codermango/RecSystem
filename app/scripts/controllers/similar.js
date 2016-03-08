4ad2782f472a219875c1cdda71ce30fd0a49544a
codermango <codermango@gmail.com>
2016-03-08 13:28:46 +0100

    Add radar chart to show the reason of the recommendation

--- app/scripts/controllers/similar.js
+++ app/scripts/controllers/similar.js
@@ -23,4 +23,50 @@ angular.module('RecSystemWebApp')
             }
             $location.path('/similar/' + imdbid + '/' + recNum + '/');
         };
+
+
+        $scope.showChart = function(e) {
+            let feature_labels = [];
+            let feature_values = [];
+            for (let key in this.features) {
+                if (key !== 'sum') {
+                    feature_labels.push(key);
+                    feature_values.push(this.features[key]);
+                }
+            }
+            $scope.labels = feature_labels;
+            $scope.data = [feature_values];
+
+            // console.log($scope.labels);
+            // console.log($scope.data);
+
+            // get the position of current div
+            let currentDiv = $("#div-" + this.movie);
+            // console.log(currentDiv.offset());
+            // console.log(currentDiv.width());
+
+            Chart.defaults.global.tooltipFontColor = "#FA6800";
+            Chart.defaults.global.tooltipFontSize = 58;
+            let chartDiv = $("#reason-chart");
+
+            chartDiv.css('display', 'block');
+            let left = currentDiv.offset()['left'] + currentDiv.outerWidth() / 2 - chartDiv.outerWidth() / 2;
+            let top = currentDiv.offset()['top'] - chartDiv.outerHeight();
+            // console.log(left + " " + top); 
+            // console.log(chartDiv.height());
+            chartDiv.offset({ top: top, left: left });
+
+
+        }
+
+        $scope.hideChart = function() {
+            $("#reason-chart").css('display', 'none');
+        }
+
+        // $scope.labels = ["genre", "actor"];
+        // $scope.data = [
+        //     [1.3199999999999998, 1.6]
+        // ];
+
+
     });
